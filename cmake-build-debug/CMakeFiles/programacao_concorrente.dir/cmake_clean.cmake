0e4addf6aab174a2d2d0ae5947e2e4fd772ae0f4
emanuelle <emanuguse@gmail.com>
2024-04-26 14:52:58 -0300

    arrumando docs

---
+++ cmake-build-debug/CMakeFiles/programacao_concorrente.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/programacao_concorrente.dir/exercicios-de-aula/04-03/Main.c.o"
+  "CMakeFiles/programacao_concorrente.dir/exercicios-de-aula/04-03/Main.c.o.d"
+  "CMakeFiles/programacao_concorrente.dir/exercicios-de-aula/04-03/MinMax.c.o"
+  "CMakeFiles/programacao_concorrente.dir/exercicios-de-aula/04-03/MinMax.c.o.d"
+  "CMakeFiles/programacao_concorrente.dir/exercicios-de-aula/04-03/SortVector.c.o"
+  "CMakeFiles/programacao_concorrente.dir/exercicios-de-aula/04-03/SortVector.c.o.d"
+  "programacao_concorrente"
+  "programacao_concorrente.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/programacao_concorrente.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/programacao_concorrente.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for programacao_concorrente.
+# This may be replaced when dependencies are built.
